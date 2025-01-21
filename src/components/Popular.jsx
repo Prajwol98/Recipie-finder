1046a82d262bfc1781b41ce1206a42500a052d6e
Prajwol Mainali <prajwolmainali96@gmail.com>
2025-01-21 20:39:47 +0545

    updates

--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -1,40 +1,7 @@
-// import React from "react";
-// import { useEffect, useState } from "react";
-
-// const Popular = () => {
-//   const [popular, setPopular] = useState();
-
-//   const getData = async () => {
-//     try {
-//       const api = await fetch(
-//         "https://www.themealdb.com/api/json/v1/1/lookup.php?i=52772"
-//       );
-//       const data = await api.json();
-//       console.log(data.meals);
-//     } catch (error) {
-//       console.log(error);
-//     }
-//   };
-//   useEffect(() => {
-//     getData();
-//   }, []);
-
-//   return (
-//     <div>
-//       <div>
-//         <h1>Popular</h1>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default Popular;
-
 import React, { useEffect, useState } from "react";
 
 const Popular = () => {
-  const [popular, setPopular] = useState(null); // Initialize state as null
-
+  const [popular, setPopular] = useState([]);
   const getData = async () => {
     try {
       const api = await fetch(
@@ -42,10 +9,10 @@ const Popular = () => {
         // "https://www.themealdb.com/api/json/v1/1/lookup.php?i=52772"
       );
       const data = await api.json();
-      console.log(data.categories.slice(0, 8)); // Log fetched data
-      // setPopular(data.meals.slice(0, 9)); // Update state with fetched data
+
+      setPopular(data.categories.slice(0, 8));
     } catch (error) {
-      console.error("Error fetching data:", error); // Log error
+      console.error("Error fetching data:", error);
     }
   };
 
@@ -56,6 +23,15 @@ const Popular = () => {
   return (
     <div>
       <h1>Popular</h1>
+      {popular.map((items) => {
+        return (
+          <div key={items.idCategory}>
+            <h2>{items.strCategory}</h2>
+            <img src={items.strCategoryThumb} alt="img" />
+            <p>{items.strCategoryDescription}</p>
+          </div>
+        );
+      })}
     </div>
   );
 };
